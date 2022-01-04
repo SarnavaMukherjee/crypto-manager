161f91da54bac417b8fe3f90ca4bbe710d97e0b7
Sarnava Mukherjee <sarnava.mukherjee@airavana.ai>
2022-01-04 11:01:34 +0530

    Initial Commit

---
+++ cmd/crypto-manager/main.go
@@ -0,0 +1,14 @@
+/**
+ * File: main.go
+ * Author: Sarnava Mukherjee
+ * Contact: (sarnavamukherjee20@gmail.com)
+ */
+ 
+package main
+
+import "github.com/SarnavaMukherjee/crypto-manager/pkg/logger"
+
+func main() {
+
+	logger.CreateLogger()
+}
---
+++ pkg/logger/logger.go
@@ -0,0 +1,129 @@
+/**
+ * File: main.go
+ * Author: Sarnava Mukherjee
+ * Contact: (sarnavamukherjee20@gmail.com)
+ */
+
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
+	"os"
+	"strings"
+)
+
+var serviceName string
+
+//CreateLogger with specific log level
+func CreateLogger(logLevel, applicationName string) {
+
+	serviceName = applicationName
+	level := getLogLevel(logLevel)
+	alevel := zap.NewAtomicLevelAt(level)
+
+	var config zap.Config
+	if level == zapcore.DebugLevel {
+		config = zap.NewDevelopmentConfig()
+	} else {
+		config = zap.NewProductionConfig()
+	}
+	config.DisableCaller = false
+	config.EncoderConfig.EncodeLevel = zapcore.LowercaseLevelEncoder
+	config.EncoderConfig.MessageKey = "message"
+	config.EncoderConfig.LevelKey = "level"
+	config.EncoderConfig.TimeKey = "time"
+	config.EncoderConfig.FunctionKey = "func"
+	config.DisableStacktrace = true
+	config.DisableCaller = true
+	config.Level = alevel
+	config.OutputPaths = []string{"stdout"}
+	config.ErrorOutputPaths = []string{"stderr"}
+	config.Encoding = "json"
+
+	logger, err := config.Build()
+	zap.ReplaceGlobals(logger)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func getLogLevel(logLevel string) zapcore.Level {
+	switch strings.ToLower(logLevel) {
+	case "debug":
+		return zapcore.DebugLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "warn":
+		return zapcore.WarnLevel
+	case "error":
+		return zapcore.ErrorLevel
+	case "dpanic":
+		return zapcore.DPanicLevel
+	case "panic":
+		return zapcore.PanicLevel
+	case "fatal":
+		return zapcore.FatalLevel
+	default:
+		return zapcore.InfoLevel
+	}
+}
+
+// GetLogger get native not sugared logger
+func GetLogger() *zap.Logger {
+	return zap.L()
+}
+
+// ObserveLogging constructs a logger through the zap/zaptest/observer framework
+// so that logs will be accessible in tests.
+func ObserveLogging(level zapcore.Level) *observer.ObservedLogs {
+	observedLogger, logs := observer.New(level)
+	logger := zap.New(observedLogger)
+	zap.ReplaceGlobals(logger)
+	return logs
+}
+
+// Debug logs a debug message with the given fields
+func Debug(tenant, tag, message string, fields ...interface{}) {
+	msg := getMessageString(tenant, tag, message)
+	zap.S().Debugf(msg, fields...)
+}
+
+// Info logs a debug message with the given fields
+func Info(tenant, tag, message string, fields ...interface{}) {
+	msg := getMessageString(tenant, tag, message)
+	zap.S().Infof(msg, fields...)
+}
+
+// Warn logs a debug message with the given fields
+func Warn(tenant, tag, message string, fields ...interface{}) {
+	msg := getMessageString(tenant, tag, message)
+	zap.S().Warnf(msg, fields...)
+}
+
+// Error logs a debug message with the given fields
+func Error(tenant, tag, message string, fields ...interface{}) {
+	msg := getMessageString(tenant, tag, message)
+	zap.S().Errorf(msg, fields...)
+}
+
+// Fatal logs a message than calls os.Exit(1)
+func Fatal(tenant, tag, message string, fields ...interface{}) {
+	msg := getMessageString(tenant, tag, message)
+	zap.S().Fatalf(msg, fields...)
+	zap.S().Sync()
+	os.Exit(1)
+}
+func getMessageString(tenant, tag, message string) string {
+	msg := "[" + serviceName + "]"
+	if len(tenant) > 0 {
+		msg += "[" + strings.ToUpper(tenant) + "]"
+	}
+	if len(tag) > 0 {
+		msg += "[" + strings.ToUpper(tag) + "]"
+	}
+	msg += message
+	return msg
+}
